8805747bf280e162f0678885cd0ee7316e014643
root <root@ubuntu.com>
2017-12-12 15:02:58 +0800

    Initial commit

---
+++ src/colordef.h
@@ -0,0 +1,36 @@
+/* Color definition for portable colors */
+
+/* Change those to fit to your MUD */
+
+/* if you do not have color at all, just change all to be "" */
+
+/* Foreground colors */
+
+#define RED		"#r"
+#define BLUE		"#l"
+#define GREEN		"#g"
+#define BLACK		""
+//#define WHITE		""
+//#define MAGENTA		"#p" /* purple? */
+//#define YELLOW		"#y"
+#define CYAN		""
+
+/* Background colors */
+
+#define RED_BG			"#R"
+#define BLUE_BG			"#L"
+#define GREEN_BG		"#G"
+#define BLACK_BG		""
+#define WHITE_BG		""
+#define MAGENTA_BG		"#P"
+#define YELLOW_BG		"#Y"
+#define CYAN_BG			""
+
+/* Other codes */
+
+#define BOLD			""
+#define NO_COLOR		"" /* Default color */
+#define BLINK       	        ""
+#define REVERSE			""
+#define TILDE         	        ""
+
